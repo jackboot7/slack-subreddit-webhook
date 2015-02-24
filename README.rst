3a1d56a3754584c6ca537ad651451eeda1735624
Luis Alberto Santana <jackboot7@gmail.com>
2015-02-24 10:13:44 -0430

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,43 @@
+Send subreddit's photos to a Slack Channel
+==========================================
+
+This small `bottle.py` applications implements a Slack Outgoing Webhook to send a group (default 15) of photos to a SlackHQ channel.
+
+To use it, you'll need to have the Imgur API's credentials, which can be obtained here.
+
+Installation
+============
+
+Local
+-----
+
+.. code:: bash
+
+   $ pip install imgurpython
+   $ pip install bottle
+
+Or
+
+.. code:: bash
+
+   $ pip install -r requirements.txt
+
+
+Then add some enviroment variables
+
+.. code:: bash
+
+    $ export IMGUR_CLIENT_ID=<your Imgur client id>
+    $ export IMGUR_CLIENT_SECRET=<your Imgur client secret>
+
+
+Heroku
+------
+
+After following the Heroku documentation to push your code, you must add three ENV variables:
+
+.. code:: bash
+    $ heroku config:set APP_LOCATION=heroku
+    $ heroku config:set IMGUR_CLIENT_ID=<your Imgur client id>
+    $ heroku config:set IMGUR_CLIENT_SECRET=<your Imgur client secret>
+
---
+++ app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*-coding:utf8 -*-
+
+import random
+import json
+import os
+
+from bottle import route, run, request, template
+from imgurpython import ImgurClient
+
+
+def get_subreddit_list(sub):
+    """
+    We can add as many subreddits as we want.
+    """
+    if sub == "food":
+        return [u'FoodPorn', u'food', u'Pizza', u'Ramen']
+
+
+def get_random_image(sub):
+    """
+    Returns 15 random images from a random subreddit based in the `sub` param.
+    """
+    client_id = os.environ.get('IMGUR_CLIENT_ID')
+    client_secret = os.environ.get('IMGUR_CLIENT_SECRET')
+
+    if client_id and client_secret:
+        client = ImgurClient(client_id, client_secret)
+
+        subreddit_list = get_subreddit_list(sub)
+        subreddit = client.subreddit_gallery(random.choice(subreddit_list))
+
+        # Uncomment this if you want just a single image to be returned.
+        # return random.choice(subreddit).link
+
+        # We'll return 15 imgur photo links, each in its own line.
+        sample = random.sample(subreddit, 15)
+        return "\n".join([item.link for item in sample])
+
+
+@route('/<sub>', method=["POST", "GET"])
+def index(sub):
+    """
+        @SlackHQ's Outgoing Webhooks require a POST request which response should be a JSON obj with a 'text' field.
+    """
+    if request.method == "POST":
+        response = {"text": get_random_image(sub)}
+        return json.dumps(response)
+    else:
+        return template("<h1>Nothing to see here just yet!</h1>")
+
+
+if __name__ == "__main__":
+    if os.environ.get('APP_LOCATION') == 'heroku':
+        run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    else:
+        run(host='localhost', port=8080, debug=True)
